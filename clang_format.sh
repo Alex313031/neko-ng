93e804b8088039241fd96211df565c82eb1b23e9
Alexander Frick <Alex313031@gmail.com>
2026-04-22 03:12:24 -0500

    fixup clang-format script

--- clang_format.sh
+++ clang_format.sh
@@ -7,8 +7,8 @@ export HERE=${PWD} &&
 export CLANG_FORMAT_FILE=${HERE}/.clang-format &&
 
 # Check cfg program first
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/NekoCFG/{*.h,*.cc,*.cpp} &&
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/NekoCFG/{*.h,*.cpp} &&
 
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/{*.h,*.cc,*.cpp}} &&
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/{*.h,*.cpp} &&
 
 exit 0
